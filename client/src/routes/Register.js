86b72563868762b4ccd2e7852a6dd162a42edb30
rohittendulkar <vrekedit00@gmail.com>
2022-03-03 21:58:37 +0530

    front end structure for form done

--- client/src/routes/Register.js
+++ client/src/routes/Register.js
@@ -5,15 +5,22 @@ export default class Register extends Component{
     constructor(props){
         super(props)
         this.state={
+            userType:'',
             firstName:'',
             lastName:'',
+            gender:'',
+            dob:'',
             emailId:'',
+            password:'',
             address:'',
             issues:'',
             upload:''
         }
     }
 
+    changeUserTypeHandler=(event)=>{
+        this.setState({userType: event.target.value});
+    }
     changeFirstNameHandler=(event)=>{
         this.setState({firstName: event.target.value});
     }
@@ -21,9 +28,21 @@ export default class Register extends Component{
     changeLastNameHandler=(event)=>{
         this.setState({lastName: event.target.value});
     }
+    
+    changeGenderHandler=(event)=>{
+        this.setState({gender: event.target.value});
+    }
+
+    changeDateHandler=(event)=>{
+        this.setState({dob: event.target.value});
+    }
 
     changeEmailIdHandler=(event)=>{
         this.setState({emailId: event.target.value});
+    }
+
+    changePasswordHandler=(event)=>{
+        this.setState({password: event.target.value})
     }
 
     changeAddressHandler=(event)=>{
@@ -37,15 +56,21 @@ export default class Register extends Component{
     addUploadHandler=(event)=>{
         this.setState({upload: event.target.value});
     }
+    
+    async registerUser(event){
+        event.preventDefault()
 
-    saveStudent=(e)=>{
-        e.preventDefault();
-        let student={firstName: this.state.firstName,lastName: this.state.lastName,emailId: this.state.emailId, address: this.state.address, issues: this.state.issues, upload: this.state.upload};
-        console.log('Student=>'+JSON.stringify(student));
-    }
+        let user={userType: this.state.userType, firstName: this.state.firstName, lastName: this.state.lastName, gender: this.state.gender, dob: this.state.dob, emailId: this.state.emailId, password: this.state.password, address: this.state.address, issues: this.state.issues, upload: this.state.upload};
+        const response = await fetch ('http://localhost:5000/api/register',{
+            method:'POST',
+            headers:{
+                'Content-Type': 'application/JSON',
+            },
+            body: JSON.stringify({user}),
+        })
+        const data = await response.json()
 
-    cancel(){
-        this.props.history.push();
+        console.log(data)
     }
 
     render(){
@@ -59,7 +84,14 @@ export default class Register extends Component{
                     <br></br>
                     <div className="card col-md-6 offset-md-3 offset-md-3">
                         <div className="card-body">
-                            <form>
+                            <form onSubmit={this.registerUser}>
+                                <div className="form-group" onChange={this.changeUserTypeHandler}>
+                                    <label>Register as:</label>
+                                    <input type="radio" value="Member" name="userType"/> Member
+                                    <input type="radio" value="Caregiver" name="userType"/> Caregiver
+                                    <input type="radio" value="Partner" name="userType"/> Partner
+                                    <input type="radio" value="Volunteer" name="userType"/> Volunteer
+                                </div>
                                 <div className="form-group">
                                     <label>First Name:</label>
                                     <input placeholder="First Name" name="firstName" className="form-control"
@@ -70,10 +102,26 @@ export default class Register extends Component{
                                     <input placeholder="Last Name" name="lastName" className="form-control"
                                     value={this.state.lastName} onChange={this.changeLastNameHandler}/>
                                 </div>
+                                <div className="form-group" onChange={this.changeGenderHandler}>
+                                    <label>Gender:</label>
+                                    <input type="radio" value="Male" name="gender"/> Male
+                                    <input type="radio" value="Female" name="gender"/> Female
+                                    <input type="radio" value="Other" name="gender"/> Other
+                                </div>
+                                <div className="form-group">
+                                    <label>Date of Birth:</label>
+                                    <input placeholder="Date of Birth" type="date" name="dob" className="form-control"
+                                    value={this.state.dob} onChange={this.changeDateHandler}/>
+                                </div>
                                 <div className="form-group">
                                     <label>Email:</label>
-                                    <input placeholder="Email" name="emailId" className="form-control"
+                                    <input placeholder="Email" type="email" name="emailId" className="form-control"
                                     value={this.state.emailId} onChange={this.changeEmailIdHandler}/>
+                                </div>
+                                <div className="form-group">
+                                    <label>Password:</label>
+                                    <input placeholder="Password" type="password" name="password" className="form-control"
+                                    value={this.state.password} onChange={this.changePasswordHandler}/>
                                 </div>
                                 <div className="form-group">
                                     <label>Address:</label>
@@ -92,8 +140,7 @@ export default class Register extends Component{
                                 </div>
                                 <br></br>
                                 <br></br>
-                                <button className="btn btn-success" onClick={this.saveStudent}>Save</button>
-                                <button className="btn btn-danger" onClick={this.cancel.bind} style={{marginLeft:"10px"}}>Cancel</button>
+                                <button className="btn btn-success" type="submit">Register</button>
                             </form>
                         </div>
                     </div>
