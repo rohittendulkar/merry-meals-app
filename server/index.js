6daa082564c8526a133d145fef02fc87e44070e5
rohittendulkar <vrekedit00@gmail.com>
2022-03-03 23:23:38 +0530

    backend update

--- server/index.js
+++ server/index.js
@@ -1,6 +1,15 @@
-const express = require('express')
-const app = express()
-const port = 5000
+const express = require("express");
+const app = express();
+const port = 5000;
+const cors = require("cors");
 
-app.get('/', (req, res) => res.send('Hello World!'))
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.use(cors());
+app.use(express.json());
+
+app.post("/api/register", (req, res) => {
+	console.log(req.body);
+	res.json({ status: "ok" });
+});
+app.listen(port, () =>
+	console.log(`Merry Meals Server listening on port ${port}!`)
+);
