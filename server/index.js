6d2220c0720b2bddd4ffcb052257d44d7490dc60
akkey-02 <akhilgkumar01@gmail.com>
2022-03-04 16:11:11 +0530

    index.js updated

--- server/index.js
+++ server/index.js
@@ -4,7 +4,8 @@ const cors = require("cors");
 const users = require("./routes/user");
 const auth = require("./routes/auth");
 
-const dotenv = require("dotenv").config();
+const dotenv = require("dotenv");
+dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -15,12 +16,12 @@ app.use("/api/register", users);
 app.use("/api/auth", auth);
 
 mongoose
-	.connect(
-		`mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@merrymealsdb.7hwao.mongodb.net/merry-meals?retryWrites=true&w=majority`
-	)
-	.then(() => console.log("Connected to MongoDB..."))
-	.catch((err) => console.error("Could not connect to MongoDB...", err));
+  .connect(
+    `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@merrymealsdb.7hwao.mongodb.net/merry-meals?retryWrites=true&w=majority`
+  )
+  .then(() => console.log("Connected to MongoDB..."))
+  .catch((err) => console.error("Could not connect to MongoDB...", err));
 
 app.listen(port, () =>
-	console.log(`Merry Meals Server listening on port ${port}!`)
+  console.log(`Merry Meals Server listening on port ${port}!`)
 );
