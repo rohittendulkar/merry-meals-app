7b5edaf789e7f0eebf672bc5f311287de5b81121
rohittendulkar <vrekedit00@gmail.com>
2022-04-07 21:25:55 +0530

    updated index.js

--- client/src/index.js
+++ client/src/index.js
@@ -7,18 +7,17 @@ import reportWebVitals from "./reportWebVitals";
 import { Register, LogIn, Home } from "./imports.js";
 
 ReactDOM.render(
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<App />} />
-      <Route path="/register" element={<Register />} />
-      <Route path="/login" element={<LogIn />} />
-      <Route path="/home" element={<Home />} />
-    </Routes>
-  </BrowserRouter>,
-  document.getElementById("root")
+	<BrowserRouter>
+		<Routes>
+			<Route path="/" element={<App />} />
+			<Route path="/register" element={<Register />} />
+			<Route path="/login" element={<LogIn />} />
+			<Route path="/home" element={<Home />} />
+		</Routes>
+	</BrowserRouter>,
+	document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
