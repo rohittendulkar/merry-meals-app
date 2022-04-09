41ab7f8005dfc17251875c6f7af2b22aa4545a92
akkey-02 <akhilgkumar01@gmail.com>
2022-04-10 01:17:57 +0530

    Redux packages added

--- client/src/App.js
+++ client/src/App.js
@@ -4,52 +4,52 @@ import ResponsiveAppBar from "./components/Navbar";
 import { About, Home, LogIn, Register } from "./imports";
 
 const theme = createTheme({
-	palette: {
-		type: "light",
-		primary: {
-			main: "#74c35e",
-		},
-		secondary: {
-			main: "#457B9D",
-			light: "#A8DADC",
-			dark: "#1D3557",
-		},
-		background: {
-			default: "#F1FAEE",
-		},
-		error: {
-			main: "#E63946",
-		},
-		warning: {
-			main: "#FFC300",
-		},
-		success: {
-			main: "#40916C",
-		},
-	},
-	typography: {
-		h1: {
-			fontFamily: "Merriweather",
-			letterSpacing: "2%",
-		},
-		fontFamily: "Oxygen",
-	},
+  palette: {
+    type: "light",
+    primary: {
+      main: "#74c35e",
+    },
+    secondary: {
+      main: "#457B9D",
+      light: "#A8DADC",
+      dark: "#1D3557",
+    },
+    background: {
+      default: "#F1FAEE",
+    },
+    error: {
+      main: "#E63946",
+    },
+    warning: {
+      main: "#FFC300",
+    },
+    success: {
+      main: "#40916C",
+    },
+  },
+  typography: {
+    h1: {
+      fontFamily: "Merriweather",
+      letterSpacing: "2%",
+    },
+    fontFamily: "Oxygen",
+  },
 });
 
 function App() {
-	return (
-		<ThemeProvider theme={theme}>
-			<BrowserRouter>
-				<ResponsiveAppBar />
-				<Routes>
-					<Route path="/" element={<Home />} />
-					<Route path="/register" element={<Register />} />
-					<Route path="/login" element={<LogIn />} />
-					<Route path="/about" element={<About />} />
-				</Routes>
-			</BrowserRouter>
-		</ThemeProvider>
-	);
+  return (
+    <ThemeProvider theme={theme}>
+      <BrowserRouter>
+        <ResponsiveAppBar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<LogIn />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </BrowserRouter>
+    </ThemeProvider>
+  );
 }
 
 export default App;
