9a28c77d1aca28b72e9b41b20672d767a04ca35b
rohittendulkar <vrekedit00@gmail.com>
2022-04-10 18:10:02 +0530

    Registration working  Update

--- server/index.js
+++ server/index.js
@@ -16,12 +16,12 @@ app.use("/api/register", users);
 app.use("/api/auth", auth);
 
 mongoose
-  .connect(
-    `mongodb+srv://admin:merrywheelsadmin123@merrymealsdb.7hwao.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
-  )
-  .then(() => console.log("Connected to MongoDB..."))
-  .catch((err) => console.error("Could not connect to MongoDB...", err));
+	.connect(
+		`mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@merrymealsdb.7hwao.mongodb.net/merry-meals?retryWrites=true&w=majority`
+	)
+	.then(() => console.log("Connected to MongoDB..."))
+	.catch((err) => console.error("Could not connect to MongoDB...", err));
 
 app.listen(port, () =>
-  console.log(`Merry Meals Server listening on port ${port}!`)
+	console.log(`Merry Meals Server listening on port ${port}!`)
 );
