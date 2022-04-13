35824171781f1f3db3132ef02422e9c204d1f04a
rohittendulkar <vrekedit00@gmail.com>
2022-04-13 23:03:18 +0530

    Navbar Login Issue fix

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -19,208 +19,209 @@ import { useSelector } from "react-redux";
 // const settings = ["Register", "Login"];
 
 const useStyles = makeStyles((theme) => ({
-  AppBar: {
-    marginBottom: "30px",
-  },
+	AppBar: {
+		marginBottom: "30px",
+	},
 }));
 
 const ResponsiveAppBar = () => {
-  const [anchorElNav, setAnchorElNav] = React.useState(null);
-  const [anchorElUser, setAnchorElUser] = React.useState(null);
+	const [anchorElNav, setAnchorElNav] = React.useState(null);
+	const [anchorElUser, setAnchorElUser] = React.useState(null);
 
-  const handleOpenNavMenu = (event) => {
-    setAnchorElNav(event.currentTarget);
-  };
-  const handleOpenUserMenu = (event) => {
-    setAnchorElUser(event.currentTarget);
-  };
+	const handleOpenNavMenu = (event) => {
+		setAnchorElNav(event.currentTarget);
+	};
+	const handleOpenUserMenu = (event) => {
+		setAnchorElUser(event.currentTarget);
+	};
 
-  const handleCloseNavMenu = () => {
-    setAnchorElNav(null);
-  };
+	const handleCloseNavMenu = () => {
+		setAnchorElNav(null);
+	};
 
-  const handleCloseUserMenu = () => {
-    setAnchorElUser(null);
-  };
+	const handleCloseUserMenu = () => {
+		setAnchorElUser(null);
+	};
 
-  const classes = useStyles();
+	const classes = useStyles();
 
-  const userState = useSelector((state) => state.loginReducer);
-  const { currentUser } = userState;
-  const { user } = currentUser;
+	const userState = useSelector((state) => state.loginReducer);
+	const { currentUser } = userState;
 
-  return (
-    <AppBar className={classes.AppBar} position="static">
-      <Container maxWidth="xl">
-        <Toolbar disableGutters>
-          <Typography
-            variant="h6"
-            noWrap
-            component="div"
-            sx={{ mr: 2, display: { xs: "none", md: "flex" } }}
-          >
-            Merry Meals
-          </Typography>
+	return (
+		<AppBar className={classes.AppBar} position="static">
+			<Container maxWidth="xl">
+				<Toolbar disableGutters>
+					<Typography
+						variant="h6"
+						noWrap
+						component="div"
+						sx={{ mr: 2, display: { xs: "none", md: "flex" } }}
+					>
+						Merry Meals
+					</Typography>
 
-          <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
-            <IconButton
-              size="large"
-              aria-label="account of current user"
-              aria-controls="menu-appbar"
-              aria-haspopup="true"
-              onClick={handleOpenNavMenu}
-              color="inherit"
-            >
-              <MenuIcon />
-            </IconButton>
-            <Menu
-              id="menu-appbar"
-              anchorEl={anchorElNav}
-              anchorOrigin={{
-                vertical: "bottom",
-                horizontal: "left",
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: "top",
-                horizontal: "left",
-              }}
-              open={Boolean(anchorElNav)}
-              onClose={handleCloseNavMenu}
-              sx={{
-                display: { xs: "block", md: "none" },
-              }}
-            >
-              <MenuItem component={Link} to="/" onClick={handleCloseNavMenu}>
-                <Typography textAlign="center">Home</Typography>
-              </MenuItem>
-              <MenuItem
-                component={Link}
-                to="/about"
-                onClick={handleCloseNavMenu}
-              >
-                <Typography textAlign="center">About</Typography>
-              </MenuItem>
-              <MenuItem
-                component={Link}
-                to="/meals"
-                onClick={handleCloseNavMenu}
-              >
-                <Typography textAlign="center">Meals</Typography>
-              </MenuItem>
-            </Menu>
-          </Box>
-          <Typography
-            variant="h6"
-            noWrap
-            component="div"
-            sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}
-          >
-            Merry Meals
-          </Typography>
-          <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
-            <Button
-              component={Link}
-              to="/"
-              onClick={handleCloseNavMenu}
-              sx={{ my: 2, color: "white", display: "block" }}
-            >
-              Home
-            </Button>
-            <Button
-              component={Link}
-              to="/about"
-              onClick={handleCloseNavMenu}
-              sx={{ my: 2, color: "white", display: "block" }}
-            >
-              About
-            </Button>
-            <Button
-              component={Link}
-              to="/meals"
-              onClick={handleCloseNavMenu}
-              sx={{ my: 2, color: "white", display: "block" }}
-            >
-              Meals
-            </Button>
-          </Box>
+					<Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
+						<IconButton
+							size="large"
+							aria-label="account of current user"
+							aria-controls="menu-appbar"
+							aria-haspopup="true"
+							onClick={handleOpenNavMenu}
+							color="inherit"
+						>
+							<MenuIcon />
+						</IconButton>
+						<Menu
+							id="menu-appbar"
+							anchorEl={anchorElNav}
+							anchorOrigin={{
+								vertical: "bottom",
+								horizontal: "left",
+							}}
+							keepMounted
+							transformOrigin={{
+								vertical: "top",
+								horizontal: "left",
+							}}
+							open={Boolean(anchorElNav)}
+							onClose={handleCloseNavMenu}
+							sx={{
+								display: { xs: "block", md: "none" },
+							}}
+						>
+							<MenuItem component={Link} to="/" onClick={handleCloseNavMenu}>
+								<Typography textAlign="center">Home</Typography>
+							</MenuItem>
+							<MenuItem
+								component={Link}
+								to="/about"
+								onClick={handleCloseNavMenu}
+							>
+								<Typography textAlign="center">About</Typography>
+							</MenuItem>
+							<MenuItem
+								component={Link}
+								to="/meals"
+								onClick={handleCloseNavMenu}
+							>
+								<Typography textAlign="center">Meals</Typography>
+							</MenuItem>
+						</Menu>
+					</Box>
+					<Typography
+						variant="h6"
+						noWrap
+						component="div"
+						sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}
+					>
+						Merry Meals
+					</Typography>
+					<Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
+						<Button
+							component={Link}
+							to="/"
+							onClick={handleCloseNavMenu}
+							sx={{ my: 2, color: "white", display: "block" }}
+						>
+							Home
+						</Button>
+						<Button
+							component={Link}
+							to="/about"
+							onClick={handleCloseNavMenu}
+							sx={{ my: 2, color: "white", display: "block" }}
+						>
+							About
+						</Button>
+						<Button
+							component={Link}
+							to="/meals"
+							onClick={handleCloseNavMenu}
+							sx={{ my: 2, color: "white", display: "block" }}
+						>
+							Meals
+						</Button>
+					</Box>
 
-          <Box sx={{ flexGrow: 0 }}>
-            <Tooltip title="Open settings">
-              <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
-              </IconButton>
-            </Tooltip>
+					<Box sx={{ flexGrow: 0 }}>
+						<Tooltip title="Open settings">
+							<IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+								<Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
+							</IconButton>
+						</Tooltip>
 
-            {user ? (
-              <Menu
-                sx={{ mt: "45px" }}
-                id="menu-appbar"
-                anchorEl={anchorElUser}
-                anchorOrigin={{
-                  vertical: "top",
-                  horizontal: "right",
-                }}
-                keepMounted
-                transformOrigin={{
-                  vertical: "top",
-                  horizontal: "right",
-                }}
-                open={Boolean(anchorElUser)}
-                onClose={handleCloseUserMenu}
-              >
-                <MenuItem
-                  component={Link}
-                  to="/register"
-                  onClick={handleCloseUserMenu}
-                >
-                  <Typography textAlign="center">{user.firstName}</Typography>
-                </MenuItem>
-                <MenuItem
-                  component={Link}
-                  to="/login"
-                  onClick={handleCloseUserMenu}
-                >
-                  <Typography textAlign="center">Logout</Typography>
-                </MenuItem>{" "}
-              </Menu>
-            ) : (
-              <Menu
-                sx={{ mt: "45px" }}
-                id="menu-appbar"
-                anchorEl={anchorElUser}
-                anchorOrigin={{
-                  vertical: "top",
-                  horizontal: "right",
-                }}
-                keepMounted
-                transformOrigin={{
-                  vertical: "top",
-                  horizontal: "right",
-                }}
-                open={Boolean(anchorElUser)}
-                onClose={handleCloseUserMenu}
-              >
-                <MenuItem
-                  component={Link}
-                  to="/register"
-                  onClick={handleCloseUserMenu}
-                >
-                  <Typography textAlign="center">Register</Typography>
-                </MenuItem>
-                <MenuItem
-                  component={Link}
-                  to="/login"
-                  onClick={handleCloseUserMenu}
-                >
-                  <Typography textAlign="center">Login</Typography>
-                </MenuItem>
-              </Menu>
-            )}
-          </Box>
-        </Toolbar>
-      </Container>
-    </AppBar>
-  );
+						{currentUser ? (
+							<Menu
+								sx={{ mt: "45px" }}
+								id="menu-appbar"
+								anchorEl={anchorElUser}
+								anchorOrigin={{
+									vertical: "top",
+									horizontal: "right",
+								}}
+								keepMounted
+								transformOrigin={{
+									vertical: "top",
+									horizontal: "right",
+								}}
+								open={Boolean(anchorElUser)}
+								onClose={handleCloseUserMenu}
+							>
+								<MenuItem
+									component={Link}
+									to="/register/member"
+									onClick={handleCloseUserMenu}
+								>
+									<Typography textAlign="center">
+										{currentUser.user.firstName} {currentUser.user.lastName}
+									</Typography>
+								</MenuItem>
+								<MenuItem
+									component={Link}
+									to="/login"
+									onClick={handleCloseUserMenu}
+								>
+									<Typography textAlign="center">Logout</Typography>
+								</MenuItem>{" "}
+							</Menu>
+						) : (
+							<Menu
+								sx={{ mt: "45px" }}
+								id="menu-appbar"
+								anchorEl={anchorElUser}
+								anchorOrigin={{
+									vertical: "top",
+									horizontal: "right",
+								}}
+								keepMounted
+								transformOrigin={{
+									vertical: "top",
+									horizontal: "right",
+								}}
+								open={Boolean(anchorElUser)}
+								onClose={handleCloseUserMenu}
+							>
+								<MenuItem
+									component={Link}
+									to="/register/member"
+									onClick={handleCloseUserMenu}
+								>
+									<Typography textAlign="center">Register</Typography>
+								</MenuItem>
+								<MenuItem
+									component={Link}
+									to="/login"
+									onClick={handleCloseUserMenu}
+								>
+									<Typography textAlign="center">Login</Typography>
+								</MenuItem>
+							</Menu>
+						)}
+					</Box>
+				</Toolbar>
+			</Container>
+		</AppBar>
+	);
 };
 export default ResponsiveAppBar;
