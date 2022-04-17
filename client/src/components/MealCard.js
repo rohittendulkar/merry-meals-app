512cc9158e297f16477d7647bf368b27dafebf60
rohittendulkar <vrekedit00@gmail.com>
2022-04-17 22:10:28 +0530

    Meal Card UI update

--- client/src/components/MealCard.js
+++ client/src/components/MealCard.js
@@ -5,11 +5,18 @@ import {
 	CardActions,
 	CardContent,
 	CardMedia,
+	Chip,
+	FormControl,
+	Grid,
+	InputLabel,
+	MenuItem,
+	Select,
 	Typography,
 } from "@mui/material";
-import React from "react";
+import React, { useState } from "react";
 
 const MealCard = ({ item }) => {
+	const [qty, setQty] = useState(1);
 	return (
 		<>
 			<Card sx={{ maxWidth: 345 }}>
@@ -27,6 +34,38 @@ const MealCard = ({ item }) => {
 						<Typography variant="body2" color="text.secondary">
 							{item.description}
 						</Typography>
+						<Grid
+							container
+							spacing={3}
+							sx={{ mt: 2 }}
+							justifyContent="space-around"
+							alignItems="center"
+						>
+							<Grid item>
+								<Chip label={item.category} />
+							</Grid>
+							<Grid item>
+								<FormControl sx={{ m: 1, minWidth: 80 }} size="small">
+									<InputLabel id="demo-simple-select-autowidth-label">
+										Qty
+									</InputLabel>
+									<Select
+										labelId="demo-simple-select-autowidth-label"
+										id="demo-simple-select-autowidth"
+										value={qty}
+										onChange={(e) => setQty(e.target.value)}
+										autoWidth
+										label="Qty"
+									>
+										{[...Array(5).keys()].map((val, i) => (
+											<MenuItem sx={{ mx: 2 }} value={i + 1}>
+												{i + 1}
+											</MenuItem>
+										))}
+									</Select>
+								</FormControl>
+							</Grid>
+						</Grid>
 					</CardContent>
 				</CardActionArea>
 				<CardActions>
