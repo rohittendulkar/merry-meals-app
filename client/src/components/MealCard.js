1c68ae86bc9b3667c68feae008c2a8cbb0a884a0
rohittendulkar <vrekedit00@gmail.com>
2022-04-18 10:27:35 +0530

    Modal Added

--- client/src/components/MealCard.js
+++ client/src/components/MealCard.js
@@ -1,4 +1,5 @@
 import {
+	Box,
 	Button,
 	Card,
 	CardActionArea,
@@ -6,11 +7,14 @@ import {
 	CardContent,
 	CardMedia,
 	Chip,
+	Divider,
 	FormControl,
 	Grid,
 	InputLabel,
 	MenuItem,
+	Modal,
 	Select,
+	Stack,
 	Typography,
 } from "@mui/material";
 import React, { useState } from "react";
@@ -18,8 +22,24 @@ import React, { useState } from "react";
 import { useDispatch } from "react-redux";
 import { addToCart } from "../redux/action/cartAction";
 
+const modalStyle = {
+	position: "absolute",
+	top: "50%",
+	left: "50%",
+	transform: "translate(-50%, -50%)",
+	width: 400,
+	bgcolor: "background.paper",
+	border: "2px solid #000",
+	boxShadow: 24,
+	p: 4,
+};
+
 const MealCard = ({ item }) => {
 	const [quantity, setQuantity] = useState(1);
+
+	const [open, setOpen] = React.useState(false);
+	const handleOpen = () => setOpen(true);
+	const handleClose = () => setOpen(false);
 	const dispatch = useDispatch();
 
 	const addToCartHandler = () => {
@@ -28,7 +48,7 @@ const MealCard = ({ item }) => {
 	return (
 		<>
 			<Card sx={{ maxWidth: 345 }}>
-				<CardActionArea>
+				<CardActionArea onClick={handleOpen}>
 					<CardMedia
 						component="img"
 						height="140"
@@ -82,6 +102,36 @@ const MealCard = ({ item }) => {
 					</Button>
 				</CardActions>
 			</Card>
+			<Modal open={open} onClose={handleClose}>
+				<Box sx={modalStyle}>
+					<Stack
+						container
+						divider={<Divider orientation="horizontal" flexItem />}
+						spacing={2}
+					>
+						<Typography variant="h6" component="h2">
+							{item.title}
+						</Typography>
+
+						<Grid container spacing={2}>
+							<Grid item xs>
+								<CardMedia
+									component="img"
+									height="140"
+									image={item.imageUrl}
+									alt={item.title}
+								/>
+							</Grid>
+							<Grid item xs>
+								<Typography sx={{ mt: 2, color: "secondary.dark" }}>
+									Description:
+								</Typography>
+								<Typography sx={{ mt: 2 }}>{item.description}</Typography>
+							</Grid>
+						</Grid>
+					</Stack>
+				</Box>
+			</Modal>
 		</>
 	);
 };
