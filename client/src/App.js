9bc5747e299b8f91919036f24efc3a4ea7aaab49
rohittendulkar <vrekedit00@gmail.com>
2022-04-19 11:34:50 +0530

    Added Cart Link to App.js

--- client/src/App.js
+++ client/src/App.js
@@ -3,63 +3,65 @@ import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Footer from "./components/Footer";
 import ResponsiveAppBar from "./components/Navbar";
 import { About, Home, LogIn, Register } from "./imports";
+import Cart from "./routes/Cart";
 import Meal from "./routes/Meal";
 import PageNotFound from "./routes/PageNotFound";
 import PartnerRegister from "./routes/PartnerRegister";
 
 const theme = createTheme({
-  palette: {
-    type: "light",
-    primary: {
-      main: "#74c35e",
-    },
-    secondary: {
-      main: "#457B9D",
-      light: "#A8DADC",
-      dark: "#1D3557",
-    },
-    background: {
-      default: "#F1FAEE",
-    },
-    error: {
-      main: "#E63946",
-    },
-    warning: {
-      main: "#FFC300",
-    },
-    success: {
-      main: "#40916C",
-    },
-  },
-  typography: {
-    h1: {
-      fontFamily: "Merriweather",
-      letterSpacing: "2%",
-    },
-    fontFamily: "Oxygen",
-  },
+	palette: {
+		type: "light",
+		primary: {
+			main: "#74c35e",
+		},
+		secondary: {
+			main: "#457B9D",
+			light: "#A8DADC",
+			dark: "#1D3557",
+		},
+		background: {
+			default: "#F1FAEE",
+		},
+		error: {
+			main: "#E63946",
+		},
+		warning: {
+			main: "#FFC300",
+		},
+		success: {
+			main: "#40916C",
+		},
+	},
+	typography: {
+		h1: {
+			fontFamily: "Merriweather",
+			letterSpacing: "2%",
+		},
+		fontFamily: "Oxygen",
+	},
 });
 
 function App() {
-  return (
-    <ThemeProvider theme={theme}>
-      <BrowserRouter>
-        <ResponsiveAppBar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/register/member" element={<Register />} />
-          <Route path="/login" element={<LogIn />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/meals" element={<Meal />} />
-          <Route path="/register/partner" element={<PartnerRegister />} />
-          <Route path="/404-not-found" element={<PageNotFound />} />
-          <Route path="*" element={<Navigate to="/404-not-found" />} />
-        </Routes>
+	return (
+		<ThemeProvider theme={theme}>
+			<BrowserRouter>
+				<ResponsiveAppBar />
+				<Routes>
+					<Route path="/" element={<Home />} />
+					<Route path="/register/member" element={<Register />} />
+					<Route path="/login" element={<LogIn />} />
+					<Route path="/about" element={<About />} />
+					<Route path="/meals" element={<Meal />} />
+					<Route path="/cart" element={<Cart />} />
+					<Route path="/register/partner" element={<PartnerRegister />} />
+					<Route path="/404-not-found" element={<PageNotFound />} />
+					<Route path="*" element={<Navigate to="/404-not-found" />} />
+				</Routes>
 
-        <Footer />
-      </BrowserRouter>
-    </ThemeProvider>
-  );
+				<Footer />
+			</BrowserRouter>
+		</ThemeProvider>
+	);
 }
 
 export default App;
